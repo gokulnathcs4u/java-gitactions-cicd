aae477ba2a2cfcd85eb1f5ea1c083c3934570c72
Gokul <gokulnathcs4u@gmail.com>
2022-02-26 13:58:02 +0530

    api changes

--- src/main/java/com/javacicd/javacicd/controller/WelcomeController.java
+++ src/main/java/com/javacicd/javacicd/controller/WelcomeController.java
@@ -1,9 +1,17 @@
 package com.javacicd.javacicd.controller;
 
-import com.javacicd.javacicd.entity.TestTodoList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.javacicd.javacicd.entity.TestTodo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,12 +21,35 @@ public class WelcomeController {
     @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping("/welcome")
-    public TestTodoList welcome() {
-        TestTodoList todoList = restTemplate.getForObject(
+    List<TestTodo> testTodos = new ArrayList<>();
+
+    @GetMapping("/todos")
+    public List<TestTodo> getAllTodos() {
+        ResponseEntity<List<TestTodo>> todoList = restTemplate.exchange(
                 "https://jsonplaceholder.typicode.com/todos",
-                TestTodoList.class);
-        return todoList;
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<TestTodo>>() {
+                });
+        testTodos = todoList.getBody();
+        return testTodos;
+    }
+
+    @GetMapping("/todos/user/{userId}")
+    public List<TestTodo> getAllTodosByUserId(@PathVariable("userId") Integer userId) {
+        if (CollectionUtils.isEmpty(testTodos)) {
+            testTodos = getAllTodos();
+        }
+        return testTodos.stream().filter(todo -> todo.getUserId().intValue() == userId.intValue())
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/todos/{id}")
+    public List<TestTodo> getAllTodosById(@PathVariable("id") Integer id) {
+        if (CollectionUtils.isEmpty(testTodos)) {
+            testTodos = getAllTodos();
+        }
+        return testTodos.stream().filter(todo -> todo.getId().intValue() == id.intValue())
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/javacicd/javacicd/entity/TestTodoList.java
+++ src/main/java/com/javacicd/javacicd/entity/TestTodoList.java
@@ -1,9 +1,15 @@
 package com.javacicd.javacicd.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestTodoList {
     List<TestTodo> todoList;
+
+    public TestTodoList() {
+        super();
+        todoList = new ArrayList<>();
+    }
 
     public List<TestTodo> getTodoList() {
         return this.todoList;
